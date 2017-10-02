8f7a87f97c3cdbc74a2cdd08b99a80aa50be4cdd
n-kimberly <kimberlyvnguyen@gmail.com>
2017-10-01 18:35:55 -0700

    Seed database with Faker
    * Install 'faker' gem and integrate into application
    * Generate 100 wikis, 10 default users, and a user of each role
    * Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+include Faker
+
+
+10.times do
+  User.create!(
+    email: Internet.email,
+    password: 'password',
+    confirmed_at: Time.now
+  )
+end
+users = User.all #  Standard by default
+
+100.times do
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Company.bs,
+    body: Lorem.paragraphs.join(" "),
+    private: false
+  )
+end
+
+wikis = Wiki.all
+
+admin = User.create!(
+  email: 'admin@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+  role: 'admin'
+)
+
+premium = User.create!(
+  email: 'premium@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+  role: 'premium'
+)
+
+standard = User.create!(
+  email: 'standard@example.com',
+  password: 'password',
+  confirmed_at: Time.now,
+  role: 'standard'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} posts created"
