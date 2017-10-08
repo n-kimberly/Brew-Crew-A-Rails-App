aaeaf374ed5bd99086a839d07563eebb8731af44
n-kimberly <kimberlyvnguyen@gmail.com>
2017-10-08 09:39:37 -0700

    Implement Stripe to Accept User Payments
    * Install Stripe and configure Stripe in application
    * Generate Charges controller to initialize and create charges
    * Update views to allow users to navigate to stripe checkout
    * Configure routes to point to Charges controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :wikis
-
   devise_for :users
   # Make this default landing page
   root 'welcome#index'
   # Allow users to visit /about instead of /welcome/about
   get 'about' => 'welcome#about'
-
+  resources :charges, only: [:new, :create]
 end
